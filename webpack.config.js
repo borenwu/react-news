ce78c84681b4117d723cf872fcd267505b430bfa
borenwu <borenwuapple@163.com>
2018-01-10 16:39:44 +0800

    third

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,22 @@ var path = require('path')
 
 module.exports = {
     context: __dirname + '/src',
-    entry:"./js/index.js",
+    entry:"./js/root.js",
     module: {
-        rules:[{
-            test: /\.js?$/,
-            exclude: /(node_modules)/,
-            loader: 'babel-loader',
-            options:{
-                presets: ['react','es2015']
+        rules:[
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                options:{
+                    presets: ['react','es2015']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader:'style-loader!css-loader'
             }
-        }]
+    ]
     },
     output: {
         path: path.resolve(__dirname, './build'),
